e4d4a220b07757c57e1782757dfda67d0c4924b0
Bob Malzan <robertmalzan@gmail.com>
2017-07-21 17:24:39 +0200

    wechsel

--- Assets/Scripts/OceanData.cs
+++ Assets/Scripts/OceanData.cs
@@ -1,14 +1,38 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using plib.Util;
 
 public class COceanData
 {
-    private List<COceanEntity> 
+    private List<COceanEntity> mi_entityList;
 
-//     public List<COceanEntity> fu_GetListOfType(EOceanEntityType _entityType)
-//     {
-//         List<COceanEntity> list = new List<COceanEntity>();
-//         foreach(var e in )
-//     }
+    public List<COceanEntity> fu_GetListOfType(EOceanEntityType _entityType)
+    {
+        List<COceanEntity> list = new List<COceanEntity>();
+        foreach(var e in mi_entityList)
+        {
+            if (e.EntityType == _entityType)
+            {
+                list.Add(e);
+            }
+        }
+
+        return list;
+    }
+
+    public void fu_CreateOcean(int _numPlayers, int _numRocks, int _numSwirls, int _numDrifts)
+    {
+        // first, distribute some rocks
+        UnityEngine.Random.InitState((int)DateTime.Now.Ticks);
+
+
+
+    }
+
+    public bool fu_IsPlaceOccupied(EOceanEntityType _type)
+    {
+        return false;
+    }
 }
