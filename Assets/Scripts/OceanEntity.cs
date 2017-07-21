3109fa6cf50b2aaa6d28b3fafd139e0312959506
Bob Malzan <robertmalzan@gmail.com>
2017-07-21 18:23:06 +0200

    merge mit andre

--- Assets/Scripts/OceanEntity.cs
+++ Assets/Scripts/OceanEntity.cs
@@ -2,10 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum EOceanEntityType {  Empty, Rock, Ship, Storm }
-
-public class COceanEntity
+namespace Ocean
 {
-    public EOceanEntityType EntityType { get; set; }
-    public int OwnerId { get; set;  }
+    public enum EOceanEntityType {  Empty, Rock, Ship, Storm }
+
+    public abstract class AOceanEntity
+    {
+        public int pu_x;
+        public int pu_y;
+
+        public AOceanEntity(int _x, int _y)
+        {
+            pu_x = _x;
+            pu_y = _y;
+        }
+
+        public abstract EOceanEntityType EntityType { get; }
+        public int OwnerId { get; set; }
+    }
 }
---
+++ Assets/Scripts/RockEntity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Ocean
+{
+    public class CRockEntity : AOceanEntity
+    {
+        public override EOceanEntityType EntityType
+        {
+            get
+            {
+                return EOceanEntityType.Rock;
+            }
+        }
+    }
+}
