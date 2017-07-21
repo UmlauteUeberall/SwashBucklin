77b47b9582c9d414c6a899581621782ac9f1e0c3
Bob Malzan <robertmalzan@gmail.com>
2017-07-21 16:21:50 +0200

    zwischenloesung

---
+++ Assets/Scripts/OceanData.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class COceanData
+{
+    private List<COceanEntity> 
+
+//     public List<COceanEntity> fu_GetListOfType(EOceanEntityType _entityType)
+//     {
+//         List<COceanEntity> list = new List<COceanEntity>();
+//         foreach(var e in )
+//     }
+}
---
+++ Assets/Scripts/OceanEntity.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EOceanEntityType {  Empty, Rock, Ship, Storm }
+
+public class COceanEntity
+{
+    public EOceanEntityType EntityType { get; set; }
+    public int OwnerId { get; set;  }
+}
